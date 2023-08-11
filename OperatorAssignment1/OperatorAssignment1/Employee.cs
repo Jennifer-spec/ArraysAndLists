484f323b3f3008be2b506e58dd86da2f4d82de7d
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-10 23:59:46 -0700

    add a new project called OperatorAssignment1

---
+++ OperatorAssignment1/OperatorAssignment1/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee em1, Employee em2)
+        {
+            if (em1.Id == em2.Id)
+            {
+                return true;//something is wrong;
+            }
+
+        }
+        public static bool operator !=(Employee em1, Employee em2)
+        {
+            if (em1.Id != em2.Id)
+            {
+                return true; // it is right.
+            }
+        }
+
+
+    }
+}
---
+++ OperatorAssignment1/OperatorAssignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.Id = 001;
+            employee1.FirstName = "Jesse";
+            employee1.LastName = "Smith";
+            employee2.Id = 002;
+            employee1.FirstName = "Amos";
+            employee1.LastName = "Steson";
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("There are two employees which has same Id.");
+            }
+            if (employee1 != employee2)
+            {
+                Console.WriteLine("Two employees have different Id.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
