31bcccb2507c78f40d8139913c7178b0288bc82d
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-10 20:14:23 -0700

    add a new project called MehodsAndObjectsAssignment1

---
+++ MethodsAndObjectsAssignment1/MethodsAndObjectsAssignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inside of the Main method, instantiate and initialize an Employee object with 
+            //the first name "Sample" and the last name "Student"
+            Employee sam = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //call the superclass method SayName on the Employee object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsAndObjectsAssignment1/MethodsAndObjectsAssignment1/employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace MethodsAndObjectsAssignment1
+{
+    public class Employee : Person
+    {
+        //Give the employee clas a property called ID and have it be of data type int
+        public int Id { get; set; }
+    }
+}
