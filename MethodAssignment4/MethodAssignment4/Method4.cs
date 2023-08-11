eb7f80af3c633c10556f90e7c85d0933363220fa
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-10 18:48:53 -0700

    add a new project called MethodAssignment4

---
+++ MethodAssignment4/MethodAssignment4/Method4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment4
+{
+    class Method4
+    {
+        //create a menthod that takes two integer as parameter
+        public void MathOperation4(int i, int j)
+        {
+            //perform one math operation with first parameter and display the second parameter
+            Console.WriteLine("the second entering integer is: " + j);
+            int result = i * 10;
+            Console.WriteLine("the first entering integer multiple 10 equal to:" + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodAssignment4/MethodAssignment4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate MathOperation4 class
+            Method4 mathOp = new Method4();
+
+            try
+            {
+                //ask the user to provide the first integer
+                Console.WriteLine("Please enter the first integer:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                //ask the user to provide the second integer
+                Console.WriteLine("Please enter the second integer:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                mathOp.MathOperation4(num1, num2);
+
+
+            }
+            catch
+            {
+                Console.WriteLine("please enter two correct integer");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
