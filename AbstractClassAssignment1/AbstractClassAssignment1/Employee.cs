f7dd2cfbedc7d869f8dd74be43c6668be87a5b8c
Jennifer-spec <fuqinghua2@gmail.com>
2023-08-10 20:31:14 -0700

    add a new project called AbstractClassAssignment1

---
+++ AbstractClassAssignment1/AbstractClassAssignment1/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment1
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        //Implement the SayName() method inside of the Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment1/AbstractClassAssignment1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment1
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //give it the method SayName()
+        public abstract void SayName();
+    }
+}
